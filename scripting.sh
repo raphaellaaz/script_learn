89b46554861ab0112c9cba84a4ce523eba33e55e
Raphael Laaz Muñoz <raphaellaaz01@gmail.com>
2023-04-24 09:39:34 -0500

    "Aprendiendo Scripting" Creacion de Script dinamico.

---
+++ scripting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Raphael Laaz Muñoz
+#
+echo 'Ingrese su nombre: '
+var=$1
+echo "mi nombre es $var"
+
+read -p 'Ingresa tu nombre: ' name
+read -p	'Ingresa tu edad : ' age
+
+echo "Hola mi nombre es $name y mi edad es $age años"
